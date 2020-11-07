fa01aaa255a34cba496e33fd65838628ebb64f40
MasterMind <mohammad.masoomy@pushe.co>
2020-11-07 08:06:47 +0330

    add basics lecture in scala

--- src/lectures/part1basics/ValuesVariablesTypes.scala
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -1,7 +1,38 @@
 package lectures.part1basics
 
-object ValuesVariablesTypes extends App{
+object ValuesVariablesTypes extends App {
 
 
-  println("Hello, scala")
+  // VALs ARE IMMUTABLE
+  val x: Int = 42
+  // x = 2 ... COMPILER ERROR
+
+  // THE TYPES OF VALs ARE OPTIONAL
+  // COMPILER CAN INFER TYPES
+  val y = 42
+  // val x: Int = "Hello, scala" ... COMPILER CONFUSED
+
+  val aString = "Hello, Scala"
+  val anotherString: String = "Goodbye"
+  val aBoolean: Boolean = false
+  val anotherBoolean: Boolean = true
+  val aChar: Char = 'a'
+  val anInt: Int = 32
+  val aShort: Short = 23232 // COMPILER WILL COMPLAIN IF THE NUMBER IS TOO BIG
+  val aLong: Long = 21232323434L // 8 BITs INSTEAD OF 4 BITs
+  val aFloat: Float = 2.3F // F AT THE END INDICATES IT'S FLOAT
+  val aDouble: Double = 2.2
+
+  // VARIABLES
+  // WORKING LESS WITH VARIABLES IN FUNCTIONAL PROGRAMMING
+  // WE ELIMINATE SIDE EFFECTS IN FUNCTIONAL PROGRAMMING
+  var aVariable = 2
+  aVariable = 3 // side effects(functional programming)
+
+  // SIDE EFFECTS
+  // * CHANGING VARIABLES
+  // * PRINTING ON THE SCREEN
+  // * ...
+  println(aVariable) // THIS IS A SIDE EFFECT
+
 }
