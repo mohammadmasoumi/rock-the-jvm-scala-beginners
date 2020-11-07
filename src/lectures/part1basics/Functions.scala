f15b4ec19e96e0ab94b03833408909e57f26192d
MasterMind <mohammad.masoomy@pushe.co>
2020-11-07 14:02:02 +0330

    add function section

--- src/lectures/part1basics/Functions.scala
+++ src/lectures/part1basics/Functions.scala
@@ -21,7 +21,28 @@ object Functions extends App {
     if (n == 1) aString
     else aString + aRepeatedFunction(aString, n - 1)
   }
+
   println(aRepeatedFunction("Hello", 3))
 
+  def anOuterFunction(firstInt: Int, secondInt: Int): Int = {
+    def anInnerFunction(innerFirstInt: Int): Int = {
+      42 * innerFirstInt
+    }
+    anInnerFunction(secondInt + firstInt)
+  }
+
+  println(anOuterFunction(32, 21))
+
+
+  def factorial(num: Int): Int = {
+    if (num == 1) 1 else num * factorial(num - 1)
+  }
+  println(factorial(5))
+
+  def fibonacci(num: Int): Int = {
+    if (num == 1) 1 else if (num == 2) 1 else if (num == 3) 2 else fibonacci(num - 1) + fibonacci(num -2)
+
+  }
+  println(fibonacci(4))
 
 }
