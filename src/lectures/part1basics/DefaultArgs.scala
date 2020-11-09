9aaadf72e75ec5856c57df668f90a17d2e32f0fe
MohammadMasoumi <mohammad.masoomy74@email.com>
2020-11-09 22:16:47 +0330

    add DefaultArgs section

---
+++ src/lectures/part1basics/DefaultArgs.scala
@@ -0,0 +1,35 @@
+package lectures.part1basics
+
+import scala.annotation.tailrec
+
+object DefaultArgs extends App {
+
+  @tailrec
+  def trFact(n: Int, acc: Int = 1): Int =
+    if (n <= 1) acc
+    else trFact(n - 1, n * acc)
+
+  val fact10 = trFact(10, 1)
+  println(fact10)
+
+  // use default args
+  println(trFact(10))
+  println(trFact(10, 2))
+
+
+  def savePicture(format: String = "jpg", width: Int = 1920, height: Int = 1080): Unit = println("Saving picture!")
+
+  // Confuse compiler
+  // savePicture(800, 800)
+
+  /*
+    1. pass in every leading arguments
+    2. name the arguments
+   */
+  savePicture("bmp")
+  savePicture("bmp", 800)
+  savePicture(format = "bmp", height = 800, width = 800)
+  savePicture(height = 800)
+
+
+}
