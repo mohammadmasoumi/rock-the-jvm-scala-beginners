12e3378841b2d09f29049bb0302e33d7999ab385
MasterMind <mohammad.masoomy@pushe.co>
2020-11-10 08:41:28 +0330

    refactor the structure

--- src/lectures/part2oop/OOBasics.scala
+++ src/lectures/part2oop/OOBasics.scala
@@ -13,28 +13,29 @@ object OOBasics extends App {
 
   println("person: ", person)
 
+  // classes represent data and functionality
+  // constructor
+  // class parameters are NOT FIELDS
+  // age and x are FIELDS, therefore we can access with dot .
+  class Person(name: String, val age: Int = 24) { // default parameters work for classes too
+    // body
+    val x: Int = 2 // field
+
+    // we can access to the name whether it's field or not
+    def greet(name: String): Unit = println(s"${this.name} says: Hello $name")
+
+    // method overloading: method with the same name different signature(inputs)
+    // name refers to the name parameter
+    def greet(): Unit = println(s"Hi, I am $name")
+
+    // multiple constructors
+    def this(name: String) = this(name, 24) // call primary constructor
+    def this() = this("Mohammad Masoumi") // call secondary constructor
+
+    println(x)
+  }
+
+
 }
 
-// constructor
-// classes represent data and functionality
 
-// age and x are FIELDS, therefore we can access with dot .
-class Person(name: String, val age: Int = 24) { // default parameters work for classes too
-  // body
-  val x: Int = 2 // field
-
-  // we can access to the name whether it's field or not
-  def greet(name: String): Unit = println(s"${this.name} says: Hello $name")
-
-  // method overloading: method with the same name different signature(inputs)
-  // name refers to the name parameter
-  def greet(): Unit = println(s"Hi, I am $name")
-
-  // multiple constructors
-  def this(name: String) = this(name, 24) // call primary constructor
-  def this() = this("Mohammad Masoumi") // call secondary constructor
-
-  println(x)
-}
-
-// class parameters are NOT FIELDS
