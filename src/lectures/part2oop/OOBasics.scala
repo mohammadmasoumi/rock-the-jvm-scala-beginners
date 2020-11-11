084d6bd50e005d357d8f18f3fb035499430061a1
MasterMind <mohammad.masoomy@pushe.co>
2020-11-11 08:44:24 +0330

    add OOBasics

--- src/lectures/part2oop/OOBasics.scala
+++ src/lectures/part2oop/OOBasics.scala
@@ -35,6 +35,103 @@ object OOBasics extends App {
     println(x)
   }
 
+  /*
+  Assignments
+
+  Novel and a writer
+
+  Writer: first name, surname, year
+      method fullname
+
+  Novel: name, year of release, author
+    - authorAge
+    - isWrittenBy(author)
+    - copy(a new year of release) = new instance of Novel
+   */
+
+  class Writer(firstName: String, surname: String, val year: Int) {
+    def fullName(): String = s"$firstName $surname"
+
+    override def toString: String = s"${fullName()}"
+  }
+
+  class Novel(name: String, year: Int, author: Writer) {
+
+    def authorAge: Int =
+      year - author.year
+
+    def isWrittenBy(author: Writer): Boolean =
+      this.author == author
+
+    def copy(newYear: Int): Novel =
+      new Novel(name, newYear, author)
+
+    override def toString: String =
+      s"$name is written by ${author.fullName()} in $year."
+  }
+
+
+  val author: Writer = new Writer("Mohammad", "Masoumi", 2000)
+  val theSameAuthor: Writer = new Writer("Mohammad", "Masoumi", 2000)
+  val anotherAuthor: Writer = new Writer("New Writer", "Surname", 1990)
+  val aNovel: Novel = new Novel("MyNewNovel", 2010, author)
+
+  println(author.fullName())
+  println(aNovel.authorAge)
+  println(aNovel.isWrittenBy(author))
+  println(aNovel.isWrittenBy(theSameAuthor)) // Equality
+  println(aNovel.isWrittenBy(anotherAuthor))
+  println(aNovel.copy(2020))
+
+  /*
+    Counter class:
+      - receives an int value
+      - method current count
+      - method to increment/decrement => new Counter
+      - overload inc/dec to receive an amount
+   */
+
+  class Counter(val count: Int = 0) {
+    def inc: Counter = {
+      println("Incrementing")
+      new Counter(count + 1)
+    } // immutability
+
+    def dec: Counter = {
+      println("Decrementing")
+      new Counter(count - 1)
+    }
+
+    def inc(count: Int): Counter =
+      if (count <= 0) this
+      else inc.inc(count - 1)
+
+    def dec(count: Int): Counter =
+      if (count <= 0) this
+      else dec.dec(count - 1)
+
+    def print: String = toString
+
+    override def toString: String = f"Count is: $count"
+  }
+
+  val counter: Counter = new Counter(1)
+  println(counter.count)
+  println(counter.inc)
+  println(counter.count)
+  println(counter.dec)
+  println(counter.count)
+  println(counter.inc(10))
+  println(counter.count)
+  println(counter.dec(10))
+  println(counter
+    .inc(20)
+    .inc
+    .dec(10)
+    .dec
+    .count
+  )
+  println(counter.inc.print)
 
 }
 
