245f5916dc7d9e0d0056f5d7d47a7ea20f336022
MohammadMasoumi <mohammad.masoomy74@email.com>
2020-11-21 01:47:31 +0330

    add HOFsCurries section

---
+++ src/lectures/part3fp/HOFsCurries.scala
@@ -0,0 +1,62 @@
+package lectures.part3fp
+
+import scala.annotation.tailrec
+
+object HOFsCurries extends App {
+
+  // superFunction type: (Int, Function) => Function
+  val superFunction: (Int, (String, (Int => Boolean)) => Int) => (Int => Int) = null
+  // higher-order function (HOF)
+
+  // map, flatMap, filter in MyList
+
+  // function that applies a function nTimes over a value x
+  // nTimes(f, n, x)
+  // nTimes(f, 3, x) = f(f(f(x))) = nTimes(f, 2, f(x)) = f(f(f(x)))
+  // nTimes(f, n, x) = f(f(....(x))) = nTimes(f, n-1, f(x))
+
+  @tailrec
+  def nTimes(f: Int => Int, n: Int, x: Int): Int =
+    if (n <= 0) x
+    else nTimes(f, n - 1, f(x))
+
+  val plusOne = (x: Int) => x + 1
+
+  println(nTimes(plusOne, 10, 1))
+  println(nTimes((x: Int) => x + 1, 10, 1))
+  println(nTimes(_ + 1, 10, 1)) // syntactic sugar
+
+  // nTimesBetter(f, n) = x => f(f(f(...f(x))))
+  // val increment10 = nTimesBetter(plusOne, 10) = x => plusOne(plusOne(plusOne(...plusOne(x))))
+  // val y = increment10(1)
+  def nTimesBetter(f: Int => Int, n: Int): Int => Int =
+    if (n <= 0) (x: Int) => x
+    else (x: Int) => nTimesBetter(f, n - 1)(f(x))
+
+  val plus10 = nTimesBetter(_ + 1, 10)
+  println(plus10(1))
+  println(nTimesBetter(_ + 1, 10)(1))
+
+  // curries functions
+  val superAdderFunction: Int => (Int => Int) = (x: Int) => (y: Int) => x + y
+  val anotherSuperAdderFunction: Int => Int => Int = (x: Int) => (y: Int) => x + y
+
+  val adder3 = superAdderFunction(3) // y => y + 3
+  println(adder3(10))
+
+  println(superAdderFunction(10)(20))
+  println(anotherSuperAdderFunction(10)(20))
+
+
+  // function with multiple parameter lists
+  def curriedFormatter(c: String)(x: Double): String = c.format(x)
+
+  // declaring types are important
+  val standardFormat: (Double => String) = curriedFormatter("%4.2f")
+  val preciseFormat: (Double => String) = curriedFormatter("%10.8f")
+
+  println(standardFormat(Math.PI))
+  println(preciseFormat(Math.PI))
+
+
+}
