96d0e6c136e4a4407519b614dbe0c110556dfe2d
MohammadMasoumi <mohammad.masoomy74@email.com>
2020-11-20 23:07:57 +0330

    add whats a function assignment

--- src/exercises/MyList.scala
+++ src/exercises/MyList.scala
@@ -65,16 +65,44 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
     if (t.isEmpty) "" + h
     else h + " " + t.printElements
 
+  /*
+    [1, 2, 3].map(n * 2)
+      = new Cons(2, [2, 3].map(n * 2))
+      = new Cons(2, new Cons(4, [3].map(n * 2)))
+      = new Cons(2, new Cons(4, new Cons(6, Empty.map(n * 2))))
+      = new Cons(2, new Cons(4, new Cons(6, Empty)))
+   */
   override def map[B](transformer: A => B): MyList[B] =
     new Cons[B](transformer(h), t.map(transformer))
 
+  /*
+    [1, 2].flatMap(n => [n, n+1])
+    = [1, 2] ++ [2].flatMap(n => [m, n+1])
+    = [1, 2] ++ [2, 3] ++ Empty.flatMap(n => [m, n+1])
+    = [1, 2] ++ [2, 3] ++ Empty
+    = [1, 2, 3, 4]
+
+   */
   override def flatMap[B](transformer: A => MyList[B]): MyList[B] =
     transformer(h) ++ t.flatMap(transformer)
 
+  /*
+    [1, 2, 3].filter(n % 2 == 0)
+     = [2, 3].filter(n % 2 == 0)
+     = new Cons(2, [3].filter(n % 2 == 0))
+     = new Cons(2, Empty.filter(n % 2 == 0))
+     = new Cons(2, Empty)
+   */
   override def filter(predicate: A => Boolean): MyList[A] =
     if (predicate(h)) new Cons(h, t.filter(predicate))
     else t.filter(predicate)
 
+  /*
+    [1, 2] ++ [3, 4, 5]
+     = new Cons(1, [2] ++ [3, 4, 5])
+     = new Cons(1, new Cons(2, Empty ++ [3, 4, 5]))
+     = new Cons(1, new Cons(2, new Cons(3, new Cons(4, new Cons(5, Empty)))))
+   */
   override def ++[B >: A](list: MyList[B]): MyList[B] = new Cons[B](h, t ++ list)
 
 }
