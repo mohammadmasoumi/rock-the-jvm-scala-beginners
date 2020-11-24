41e77125cb6cb55561f188cf7d83b1ab9bfe20f4
MasterMind <mohammad.masoomy@pushe.co>
2020-11-24 23:09:53 +0330

    finish Optional

--- src/lectures/part4patterns/Options.scala
+++ src/lectures/part4patterns/Options.scala
@@ -1,5 +1,15 @@
 package lectures.part4patterns
 
+import scala.util.Random
+
+/*
+Imperative programming:
+
+  In computer science, imperative programming is a programming paradigm that uses statements
+  that change a program's state. In much the same way that the imperative mood in natural languages
+  expresses commands, an imperative program consists of commands for the computer to perform.
+ */
+
 object Options extends App {
   val myFirstOption: Option[Int] = Some(4)
   val noOption: Option[Int] = None
@@ -30,6 +40,8 @@ object Options extends App {
 
   val betterChainedResult = betterBackupMethod() orElse betterBackupMethod()
 
+  def playgroundBackupMethod(): Option[String] = Some("A valid result!") // we should use `Some`
+
   println(betterChainedResult)
 
   // functions on Options
@@ -43,5 +55,68 @@ object Options extends App {
 
   // because we have map, flatMap, filter -> we will have for-comprehension
   // for-comprehension
+
+  /*
+    Exercise.
+   */
+  val config: Map[String, String] = Map(
+    "host" -> "176.10.10.20",
+    "port" -> "80"
+  )
+
+  class Connection {
+    def connect = "Connected" // Connect to some server
+  }
+
+  object Connection {
+    val random = new Random(System.nanoTime())
+
+    def apply(host: String, port: String): Option[Connection] =
+      if (random.nextBoolean()) Some(new Connection)
+      else None
+  }
+
+  // try to establish a connection, if so - print the connect method
+
+  // first approach
+  val host = config.get("host")
+  val port = config.get("port")
+
+  /*
+    if (h != null)
+      if (p != null)
+        return Connection(h, p)
+    return null
+   */
+  val connection = host.flatMap(h => port.flatMap(p => Connection(host = h, port = p)))
+  /*
+    if (c != null)
+      return c.connect
+    return null
+   */
+  val connectStatus = connection.map(c => c.connect)
+  // if (connectionStatus == null) println(None) else println(Some(connectionStatus.get))
+  println(s"Connection status: $connectStatus")
+  /*
+    if (connectionStatus != null)
+      println(connectionStatus)
+   */
+  connectStatus.foreach(println)
+
+  // chain calls
+  config.get("host")
+    .flatMap(h => config.get("port")
+      .flatMap(p => Connection(h, p))
+      .map(connection => connection.connect))
+    .foreach(println)
+
+  // for-comprehension
+  val forConnectionStatus = for (
+    host <- config.get("host");
+    port <- config.get("port");
+    connection <- Connection(host, port)
+  ) yield connection.connect
+  forConnectionStatus.foreach(println)
+
 
 }
