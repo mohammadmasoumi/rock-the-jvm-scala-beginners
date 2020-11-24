4b4226a71b414f9ddfebf59b35e9335b021f699e
Mohammad Masoumi <mohammad.masoomy74@gmail.com>
2020-11-25 00:49:40 +0330

    add nestedTuple matching

--- src/lectures/part4pm/AllThePatterns.scala
+++ src/lectures/part4pm/AllThePatterns.scala
@@ -20,4 +20,28 @@ object AllThePatterns extends App {
 
   println(matchAnyThing)
 
+  // 2.2 variable
+  val matchAVariable = x match {
+    case something => s"I've found $something"
+  }
+
+  println(matchAnyThing)
+
+  // 3. tuples
+  val aTuple = (1, 2)
+  val matchATuple = aTuple match {
+    case (1, 1) =>
+    case (something, 2) => s"Half match $something"
+  }
+  println(matchATuple)
+
+  val aNestedTuple = (1, (1, 2))
+  val matchANestedTuple = aNestedTuple match {
+    case (firstElement, (firstNestedElement, secondNestedElement)) =>
+      s"($firstElement, ($firstNestedElement, $secondNestedElement))"
+    case _ => "Nothing"
+  }
+  println(matchANestedTuple)
+
+
 }
