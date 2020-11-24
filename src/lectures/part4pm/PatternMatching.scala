5d58d2d3808ac00546d05cbf8cc9bef82dae4e00
MasterMind <mohammad.masoomy@pushe.co>
2020-11-25 00:19:00 +0330

    add exercise

--- src/lectures/part4pm/PatternMatching.scala
+++ src/lectures/part4pm/PatternMatching.scala
@@ -43,7 +43,7 @@ object PatternMatching extends App {
       1. Cases are match in order.
       2. What if no cases match? MatchError
       3. What is the type of PM expression? Unifying all types and return the lowest common ancestor
-
+      4. PM works really well with the case classes.
    */
   val greeting = mohammad match {
     // Guard => if age < 25
@@ -62,11 +62,57 @@ object PatternMatching extends App {
 
   case class Parrot(greeting: String) extends Animal
 
-  val animal: Animal = new Dog("k9")
+  val animal: Animal = Dog("k9")
 
-  //  animal match {
-  //    case
-  //  }
+  animal match {
+    case Dog(someBread) => println(s"Matched a dog of the $someBread")
+  }
+
+  // match everything
+  val isEven = x match {
+    case n if n % 2 == 0 => true
+    case _ => false
+  } // Why would you do that? OVER KILL
+  val isEvenCond = if (x % 2 == 0) true else false // ??
+  val normalIsEven = x % 2 == 0 // return to your root
+  println(normalIsEven)
+
+  /*
+    Exercise
+      simple function uses PM
+        take an expression and return human readable form
+
+        Sum(Number(2), Number(3)) => 2 + 3
+        Sum(Sum(Number(2), Number(3)), Number(4)) => 2 + 3 + 4
+        Prod(Sum(Number(2), Number(1)), Number(3)) => 2 * 1 + 3
+   */
+
+  trait Expr
+
+  case class Number(n: Int) extends Expr
+
+  case class Sum(s1: Expr, s2: Expr) extends Expr
+
+  case class Prod(s1: Expr, s2: Expr) extends Expr
+
+  def show(e: Expr): String = e match {
+    case Number(n) => s"$n"
+    case Sum(s1, s2) => show(s1) + " + " + show(s2)
+    case Prod(s1, s2) => {
+      def maybeShowParenthesis(exp: Expr) = exp match {
+        case Prod(_, _) => show(exp)
+        case Number(_) => show(exp)
+        case _ => "(" + show(exp) + ")"
+      }
+
+      maybeShowParenthesis(s1) + " * " + maybeShowParenthesis(s2)
+    }
+  }
+
+  println(show(Sum(Number(2), Number(3))))
+  println(show(Sum(Sum(Number(2), Number(3)), Number(4))))
+  println(show(Prod(Sum(Number(2), Number(1)), Number(3))))
+  println(show(Prod(Sum(Number(2), Number(1)), Sum(Number(3), Number(4)))))
 
 
 }
