7aa86f9673a1e9394631589d728dd8f1206ecb86
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-11-26 13:52:49 +0330

    add pattern matching everywhere section

--- src/lectures/part4pm/PatternsEveryWhere.scala
+++ src/lectures/part4pm/PatternsEveryWhere.scala
@@ -29,7 +29,6 @@ object PatternsEveryWhere extends App {
   val evenOnes = for {
     x <- list if x % 2 == 0
   } yield 10 * x
-
   println(evenOnes)
 
   // generators are also base on PM
@@ -37,7 +36,36 @@ object PatternsEveryWhere extends App {
   val filterTuples = for (
     (first, second) <- myTuples
   ) yield first * second
-
   println(filterTuples)
+
+  // big idea #3
+  val tuple = (1, 2, 3)
+  val (a, b, c) = tuple // like python
+  println(s"a: $a, b: $b, c: $c")
+
+  // multiple value definitions based on PATTERN MATCHING
+  // ALL THE POWER ARE AVAILABLE
+
+  val head :: tail = list
+  println(s"head: $head, tail: $tail")
+
+  // nig idea #4 - NEW
+  // partial function based on pattern matching
+  val mappedList = list.map {
+    case v if v % 2 == 0 => v + "is even"
+    case 1 => "The One"
+    case _ => "default"
+  } // pattern matching anonymous function or partial function literal
+  mappedList.foreach(println)
+  // equivalent
+
+  val mappedList2 = list.map(x => x match {
+    case v if v % 2 == 0 => v + "is even"
+    case 1 => "The One"
+    case _ => "default"
+  })
+  mappedList2.foreach(println)
+
+
 
 }
