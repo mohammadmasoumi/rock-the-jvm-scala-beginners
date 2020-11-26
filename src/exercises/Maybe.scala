6d0e00601e1c18b929068a057a71c95eccd3cb21
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-11-26 16:50:50 +0330

    add Just collection

---
+++ src/exercises/Maybe.scala
@@ -0,0 +1,42 @@
+package exercises
+
+abstract class Maybe[+T] {
+
+  def map[B](f: T => B): Maybe[B]
+
+  def flatMap[B](f: T => Maybe[B]): Maybe[B]
+
+  def filter(p: T => Boolean): Maybe[T]
+
+
+}
+
+case object MaybeNot extends Maybe {
+  override def map[B](f: Nothing => B): Maybe[B] = MaybeNot
+
+  override def flatMap[B](f: Nothing => Maybe[B]): Maybe[B] = MaybeNot
+
+  override def filter(p: Nothing => Boolean): Maybe[Nothing] = MaybeNot
+}
+
+case class Just[+T](value: T) extends Maybe[T] {
+  override def map[B](f: T => B): Maybe[B] = Just(f(value))
+
+  override def flatMap[B](f: T => Maybe[B]): Maybe[B] = f(value)
+
+  override def filter(p: T => Boolean): Maybe[T] =
+    if (p(value)) this
+    else MaybeNot
+}
+
+
+object MaybeTest extends App {
+
+  val just3 = Just(3)
+
+  println(just3)
+  println(just3.map(_ * 2))
+  println(just3.flatMap(x => Just(x % 2 == 0)))
+  println(just3.filter(_ % 2 == 0))
+
+}
