3c89849700c5ee6be7359a4c3f33c4e67d01846f
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-11-27 22:40:55 +0330

    add sequemce functionality

--- src/lectures/part3fp/Sequences.scala
+++ src/lectures/part3fp/Sequences.scala
@@ -7,6 +7,19 @@ object Sequences extends App {
     Seq - general interface for data structures
       - have a well defined order
       - can be indexed
+
+    Support various operation
+      - apply, iterator, length, reverse, indexing, reversing
+      - concatenation, appending, prepending
+      - grouping, sorting, zipping, searching, slicing
    */
+
+  // Seq
+  val aSequence = Seq(1, 2, 3, 4) // apply constructor -> can apply other subtypes
+  println(aSequence)
+  println(aSequence.reverse)
+  println(aSequence(2)) // apply method
+  println(aSequence ++ Seq(5, 6, 7))
+
 
 }
