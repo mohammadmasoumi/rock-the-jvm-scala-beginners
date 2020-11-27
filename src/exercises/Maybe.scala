512c5607fbc86698322964c643a6fc94ac90f981
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-11-27 22:34:17 +0330

    remove extra lines and add sequnce section

--- src/exercises/Maybe.scala
+++ src/exercises/Maybe.scala
@@ -8,10 +8,10 @@ abstract class Maybe[+T] {
 
   def filter(p: T => Boolean): Maybe[T]
 
-
 }
 
 case object MaybeNot extends Maybe {
+
   override def map[B](f: Nothing => B): Maybe[B] = MaybeNot
 
   override def flatMap[B](f: Nothing => Maybe[B]): Maybe[B] = MaybeNot
@@ -20,6 +20,7 @@ case object MaybeNot extends Maybe {
 }
 
 case class Just[+T](value: T) extends Maybe[T] {
+
   override def map[B](f: T => B): Maybe[B] = Just(f(value))
 
   override def flatMap[B](f: T => Maybe[B]): Maybe[B] = f(value)
---
+++ src/lectures/part3fp/Sequences.scala
@@ -0,0 +1,5 @@
+package lectures.part3fp
+
+object Sequences extends App {
+
+}
