7def95e6f0ef55676bafd9aa12048cdf377b5938
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-11-27 22:36:19 +0330

    add seq definition

--- src/lectures/part3fp/Sequences.scala
+++ src/lectures/part3fp/Sequences.scala
@@ -2,4 +2,11 @@ package lectures.part3fp
 
 object Sequences extends App {
 
+
+  /*
+    Seq - general interface for data structures
+      - have a well defined order
+      - can be indexed
+   */
+
 }
