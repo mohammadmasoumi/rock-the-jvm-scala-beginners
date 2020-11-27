242edf7be53aef6f6c581d0cd409adfd9040e6d8
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-11-27 22:50:11 +0330

    add list definition

--- src/lectures/part3fp/Sequences.scala
+++ src/lectures/part3fp/Sequences.scala
@@ -21,5 +21,31 @@ object Sequences extends App {
   println(aSequence(2)) // apply method
   println(aSequence ++ Seq(5, 6, 7))
 
+  val unsortedSequence = Seq(2, 1, 3, 4, 6, 5, 9, 8, 7)
+  println(unsortedSequence.sorted) // no parameter (could have parameters)
+
+  // range -> Also is Seq
+  val aRange: Seq[Int] = 1 to 10 // inclusive
+  aRange.foreach(println)
+
+  // until
+  val aRangeWithUntil = 1 until 10 // exclusive
+  aRangeWithUntil.foreach(println)
+
+  // do sth 10 times
+  (1 to 10).foreach(println)
+
+  /*
+    List - A LinearSeq immutable linked list
+      - head, tail, isEmpty methods are fast O(1)
+      - most operations are O(n)L length, reverse
+
+    Sealed - ahas two subtypes
+      - object Nil (empty)
+      - class ::
+   */
+
+  // prepending and appending
+  val aList = List(1, 2, 3, 4)
 
 }
