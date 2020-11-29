ca1d5f93adb7e8e87e388a4afdb4913ee3f09b03
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2020-11-29 23:24:21 +0330

    add friend method

--- src/lectures/part3fp/TuplesAndMaps.scala
+++ src/lectures/part3fp/TuplesAndMaps.scala
@@ -87,5 +87,11 @@ object TuplesAndMaps extends App {
   def add(network: Map[String, List[String]], person: String): Map[String, List[String]] =
     network + (person -> List())
 
+  def friend(network: Map[String, List[String]], a: String, b: String): Map[String, List[String]] = {
+    val friendA = network(a)
+    val friendB = network(b)
+
+    network + (a -> (friendA + b)) + (b -> friendB + a)
+  }
 
 }
